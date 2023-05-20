b7069587e146dda0b02b37f5cde0e8ae2f9c0b1b
AdemKorajlic <ademkorajlic00@gmail.com>
2023-05-20 11:12:00 +0200

    Logging Commit

--- weather_app/weather_api/views.py
+++ weather_app/weather_api/views.py
@@ -1,15 +1,28 @@
 import requests
+import logging
 from django.http import JsonResponse
 
 WEATHERBIT_API_KEY = '1d9546c0a8d644719d74176fec92b35a'
 
+logging.basicConfig(filename='weather_api.log', level=logging.INFO)
+
+def log_request(request):
+    logging.info(f'Request - Method: {request.method}, Path: {request.path}, Params: {request.GET.dict()}')    # Log request details
+
+def log_response(response):
+    logging.info(f'Response - Status: {response.status_code}, Content: {response.content.decode()}')    # Log response details
+
 def current_weather(request):
     location = request.GET.get('location')
     if not location:
+        log_request(request)
         return JsonResponse({'error': 'Location parameter is required.'}, status=400)
 
     url = f'https://api.weatherbit.io/v2.0/current?city={location}&key={WEATHERBIT_API_KEY}'
     response = requests.get(url)
+    log_request(request)
+    log_response(response)
+
     if response.status_code == 200:
         data = response.json()
         return JsonResponse(data)
@@ -19,10 +32,14 @@ def current_weather(request):
 def weather_forecast(request):
     location = request.GET.get('location')
     if not location:
+        log_request(request)
         return JsonResponse({'error': 'Location parameter is required.'}, status=400)
 
     url = f'https://api.weatherbit.io/v2.0/forecast/daily?city={location}&key={WEATHERBIT_API_KEY}'
     response = requests.get(url)
+    log_request(request)
+    log_response(response)
+
     if response.status_code == 200:
         data = response.json()
         return JsonResponse(data)
@@ -34,10 +51,14 @@ def weather_history(request):
     start_date = request.GET.get('start')
     end_date = request.GET.get('end')
     if not location or not start_date or not end_date:
+        log_request(request)
         return JsonResponse({'error': 'Location, start date, and end date parameters are required.'}, status=400)
 
     url = f'https://api.weatherbit.io/v2.0/history/daily?city={location}&start_date={start_date}&end_date={end_date}&key={WEATHERBIT_API_KEY}'
     response = requests.get(url)
+    log_request(request)
+    log_response(response)
+
     if response.status_code == 200:
         data = response.json()
         return JsonResponse(data)
