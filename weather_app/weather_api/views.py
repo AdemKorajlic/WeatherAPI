184d67ed27b14057d561b81290a4283646e1be1e
AdemKorajlic <ademkorajlic00@gmail.com>
2023-05-21 02:39:50 +0200

    Cache Update Commit

--- weather_app/weather_api/views.py
+++ weather_app/weather_api/views.py
@@ -1,22 +1,33 @@
 import requests
 import logging
 from django.http import JsonResponse
+from django.core.cache import cache
+from datetime import datetime
 
 WEATHERBIT_API_KEY = '1d9546c0a8d644719d74176fec92b35a'
+CACHE_EXPIRATION = 600  # Cache timeout in sec
 
 logging.basicConfig(filename='weather_api.log', level=logging.INFO)
 
 def log_request(request):
-    logging.info(f'Request - Method: {request.method}, Path: {request.path}, Params: {request.GET.dict()}')    # Log request details
+    # Log request details
+    logging.info(f'Request - Method: {request.method}, Path: {request.path}, Params: {request.GET.dict()}')
 
 def log_response(response):
-    logging.info(f'Response - Status: {response.status_code}, Content: {response.content.decode()}')    # Log response details
+    # Log response details
+    logging.info(f'Response - Status: {response.status_code}, Content: {response.content.decode()}')
 
 def current_weather(request):
     location = request.GET.get('location')
     if not location:
         log_request(request)
         return JsonResponse({'error': 'Location parameter is required.'}, status=400)
+
+    cache_key = f'current_weather_{location}'
+    cached_data = cache.get(cache_key)
+
+    if cached_data:
+        return JsonResponse(cached_data)
 
     url = f'https://api.weatherbit.io/v2.0/current?city={location}&key={WEATHERBIT_API_KEY}'
     response = requests.get(url)
@@ -25,6 +36,14 @@ def current_weather(request):
 
     if response.status_code == 200:
         data = response.json()
+
+        # Add last refresh datetime to response
+        data['last_refresh'] = cache.get(cache_key + '_refreshed')
+
+        # Store data in cache
+        cache.set(cache_key, data, CACHE_EXPIRATION)
+        cache.set(cache_key + '_refreshed', datetime.now(), CACHE_EXPIRATION)
+
         return JsonResponse(data)
     else:
         return JsonResponse({'error': 'Unable to fetch current weather data.'}, status=500)
@@ -35,6 +54,12 @@ def weather_forecast(request):
         log_request(request)
         return JsonResponse({'error': 'Location parameter is required.'}, status=400)
 
+    cache_key = f'weather_forecast_{location}'
+    cached_data = cache.get(cache_key)
+
+    if cached_data:
+        return JsonResponse(cached_data)
+
     url = f'https://api.weatherbit.io/v2.0/forecast/daily?city={location}&key={WEATHERBIT_API_KEY}'
     response = requests.get(url)
     log_request(request)
@@ -42,6 +67,12 @@ def weather_forecast(request):
 
     if response.status_code == 200:
         data = response.json()
+
+        data['last_refresh'] = cache.get(cache_key + '_refreshed')
+
+        cache.set(cache_key, data, CACHE_EXPIRATION)
+        cache.set(cache_key + '_refreshed', datetime.now(), CACHE_EXPIRATION)
+
         return JsonResponse(data)
     else:
         return JsonResponse({'error': 'Unable to fetch weather forecast data.'}, status=500)
@@ -54,6 +85,12 @@ def weather_history(request):
         log_request(request)
         return JsonResponse({'error': 'Location, start date, and end date parameters are required.'}, status=400)
 
+    cache_key = f'weather_history_{location}_{start_date}_{end_date}'
+    cached_data = cache.get(cache_key)
+
+    if cached_data:
+        return JsonResponse(cached_data)
+
     url = f'https://api.weatherbit.io/v2.0/history/daily?city={location}&start_date={start_date}&end_date={end_date}&key={WEATHERBIT_API_KEY}'
     response = requests.get(url)
     log_request(request)
@@ -61,6 +98,12 @@ def weather_history(request):
 
     if response.status_code == 200:
         data = response.json()
+
+        data['last_refresh'] = cache.get(cache_key + '_refreshed')
+
+        cache.set(cache_key, data, CACHE_EXPIRATION)
+        cache.set(cache_key + '_refreshed', datetime.now(), CACHE_EXPIRATION)
+
         return JsonResponse(data)
     else:
         return JsonResponse({'error': 'Unable to fetch weather history data.'}, status=500)
